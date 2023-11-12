0b0ef5978f8be8ad2b8a3c2f2d774d5c4b5e4c25
Ivan Iochev <I.IOCHEV@rimsoft.bg>
2023-11-12 14:33:26 +0200

    move out vms

--- ArctisVoiceMeeter/ViewModels/HeadsetStatusListViewModel.cs
+++ ArctisVoiceMeeter/ViewModels/HeadsetStatusListViewModel.cs
@@ -6,39 +6,6 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ArctisVoiceMeeter.ViewModels
 {
-    public partial class HeadsetViewModel : ObservableObject
-    {
-        [ObservableProperty] private int _index;
-        [ObservableProperty] private ArctisStatus _status;
-        [ObservableProperty] private HeadsetChannelBindingViewModel[] _channelBindings;
-
-        private readonly ChannelBindingService _bindingService;
-
-        public HeadsetViewModel(int index, ArctisStatus status, ChannelBindingService bindingService)
-        {
-            _index = index;
-            _status = status;
-            _bindingService = bindingService;
-
-            _bindingService.BindingsChanged += (_,_) => LoadChannelBindings();            
-            
-            LoadChannelBindings();
-        }
-
-        private void LoadChannelBindings()
-        {
-            ChannelBindings = _bindingService.GetHeadsetBindings(Index)
-                .Select(x=>new HeadsetChannelBindingViewModel(x)).ToArray();
-        }
-
-        public string HeadsetName => $"Headset {Index + 1}";
-
-        public void UpdateStatus(ArctisStatus status)
-        {
-            Status.CopyFrom(status);
-        }
-    }
-
     public partial class HeadsetStatusListViewModel : ObservableObject
     {
         private readonly ChannelBindingService _bindingService;
--- ArctisVoiceMeeter/ViewModels/MainViewModel.cs
+++ ArctisVoiceMeeter/ViewModels/MainViewModel.cs
@@ -1,62 +1,15 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Dynamic;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
-using System.Windows.Data;
 using ArctisVoiceMeeter.Infrastructure;
-using ArctisVoiceMeeter.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 
 namespace ArctisVoiceMeeter.ViewModels;
-
-public partial class ChannelBindingListViewModel : ObservableObject
-{
-    private readonly ChannelBindingService _bindingService;
-    private ObservableCollection<ChannelBindingViewModel> _channelBindingsSource;
-
-    public ChannelBindingListViewModel(ChannelBindingService bindingService)
-    {
-        _bindingService = bindingService;
-        ChannelBindings = CreateChannelBindingsCollectionView();
-    }
-
-    public ICollectionView ChannelBindings { get; }
-
-    [RelayCommand]
-    public void CreateBinding()
-    {
-        var binding = _bindingService.AddNewBinding();
-        _channelBindingsSource.Add(new ChannelBindingViewModel(binding));
-    }
-
-    [RelayCommand]
-    public void RemoveBinding(ChannelBindingViewModel binding)
-    {
-        if(_bindingService.RemoveBinding(binding.BindingName))
-            _channelBindingsSource.Remove(binding);
-    }
-
-    [RelayCommand]
-    public void RenameBinding(ChannelBindingViewModel binding)
-    {
-
-    }
-
-    private ICollectionView CreateChannelBindingsCollectionView()
-    {
-        var viewModels = _bindingService.Bindings.Select(x => new ChannelBindingViewModel(x));
-        _channelBindingsSource = new ObservableCollection<ChannelBindingViewModel>(viewModels);
-        return CollectionViewSource.GetDefaultView(_channelBindingsSource);
-    }
-}
 
 public partial class MainViewModel : ObservableObject
 {
